09595fe0fc4f6265494d5bad3541efa8b2abcc11
belindairakoze37 <belindairakoze37@gmail.com>
2026-06-04 12:33:41 +0300

    "added register file"

--- assessment_project/web/views.py
+++ assessment_project/web/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import PassengerForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = PassengerForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Form has been submitted successfully!")
+            return redirect("register")  # reloads the form page cleanly
+        else:
+            messages.error(request, "Please correct the errors below.")
+    else:
+        form = PassengerForm()
+
+    return render(request, "register.html", {"form": form})
